c1b457648ed6c1a72e83a8c8919f9eb93d46183d
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-27 14:49:15 +0200

    done

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -2,12 +2,20 @@ package com.epam.rd.autotasks.intersection;
 
 public class Line {
 
-    public Line(int k, int b) {
+    public final double k, b;
 
+    public Line(int k, int b) {
+        this.k = k;
+        this.b = b;
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        double x = (other.b - this.b) / (this.k - other.k);
+        double y = this.k * x + this.b;
+        if ((this.k == other.k && this.b != other.b) || (this.k == other.k && this.b == other.b)) {
+            return null;
+        } else {
+            return new Point((int) x, (int) y);
+        }
     }
-
 }
--- src/main/java/com/epam/rd/autotasks/intersection/Point.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Point.java
@@ -11,6 +11,7 @@ public class Point {
 
     @Override
     public String toString() {
+
         return String.format("(%d;%d)", x, y);
     }
 }
